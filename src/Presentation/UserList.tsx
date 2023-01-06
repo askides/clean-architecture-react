e8b7a07d8cac78c12cea72f17810869664056a72
itsrennyman <renato.pozzi.dev@gmail.com>
2023-01-06 23:09:45 +0100

    2 files modified, 1 added

--- src/Presentation/UserList.tsx
+++ src/Presentation/UserList.tsx
@@ -1,52 +1,25 @@
-import { User } from "../Domain/Models/User";
-import * as React from "react";
+import { useApi } from "../Services/useApi";
 import { GetUsers } from "../Domain/UseCases/GetUsers";
 import { UserRepositoryImpl } from "../Data/Repositories/UserRepositoryImpl";
 import { UserDataSourceImpl } from "../Data/DataSources/UserDataSource";
-import { withAsync } from "../Utils/withAsync";
-
-type ApiStatus = "IDLE" | "PENDING" | "SUCCESS" | "ERROR";
-
-export function useViewModel() {
-  const [data, setData] = React.useState<User[]>([]);
-  const [status, setStatus] = React.useState<ApiStatus>("IDLE");
-
-  const getUsers = React.useCallback(async () => {
-    setStatus("PENDING");
-    const { response, error } = await withAsync(() =>
-      GetUsers(new UserRepositoryImpl(new UserDataSourceImpl()))
-    );
-
-    if (error) {
-      setStatus("ERROR");
-    } else if (response) {
-      setData(response);
-      setStatus("SUCCESS");
-    }
-  }, []);
-
-  return {
-    users: data,
-    getUsers,
-    status,
-  };
-}
 
 export function UserList() {
-  const { users, getUsers, status } = useViewModel();
-
-  React.useEffect(() => {
-    getUsers();
-  }, []);
+  // TODO: Check if it works correctly.
+  const {
+    data: users,
+    isPending,
+    isSuccess,
+    isError,
+  } = useApi(() => GetUsers(new UserRepositoryImpl(new UserDataSourceImpl())));
 
   return (
     <fieldset>
       <legend>User List</legend>
-      {status === "PENDING" ? (
-        <div>Loading...</div>
-      ) : (
+      {isPending && <div>Loading...</div>}
+      {isError && <div>Whoops... something goes wrong.</div>}
+      {isSuccess && (
         <ul>
-          {users.map((user) => (
+          {users?.map((user) => (
             <li key={user.id}>
               {user.username} - {user.email}
             </li>
---
+++ src/Services/useApi.ts
@@ -0,0 +1,50 @@
+import * as React from "react";
+import { ERROR, PENDING, SUCCESS, useApiStatus } from "../Services/Http";
+
+interface UseApiConfig<T> {
+  initialData?: T;
+}
+
+type ApiFunction<T = unknown> = (...args: unknown[]) => T | Promise<T>;
+
+export function useApi<TData = unknown, TError = unknown>(
+  fn: ApiFunction<TData>,
+  config: UseApiConfig<TData> = {}
+) {
+  const { initialData } = config;
+  const [data, setData] = React.useState<TData | undefined>(initialData);
+  const [error, setError] = React.useState<TError | unknown>();
+  const { status, setStatus, ...normalizedStatuses } = useApiStatus();
+
+  const exec = async <A>(...args: A[]) => {
+    try {
+      setStatus(PENDING);
+      const data = await fn(...args);
+      setData(data);
+      setStatus(SUCCESS);
+
+      return {
+        data,
+        error: null,
+      };
+    } catch (error) {
+      setError(error);
+      setStatus(ERROR);
+
+      return {
+        error,
+        data: null,
+      };
+    }
+  };
+
+  return {
+    data,
+    setData,
+    status,
+    setStatus,
+    error,
+    exec,
+    ...normalizedStatuses,
+  };
+}
