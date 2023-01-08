c62e57ee75a18d21b67947dbaa2b05dc3e8069cc
itsrennyman <renato.pozzi.dev@gmail.com>
2023-01-08 16:35:56 +0100

    1 file modified

--- src/Services/Http.ts
+++ src/Services/Http.ts
@@ -1,5 +1,4 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
-import * as React from "react";
 
 const instance = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com",
@@ -27,53 +26,4 @@ const api = (axios: AxiosInstance) => {
   };
 };
 
-export type ApiStatus = "IDLE" | "PENDING" | "SUCCESS" | "ERROR";
-
 export const Http = api(instance);
-export const IDLE: ApiStatus = "IDLE";
-export const PENDING: ApiStatus = "PENDING";
-export const SUCCESS: ApiStatus = "SUCCESS";
-export const ERROR: ApiStatus = "ERROR";
-
-export const defaultApiStatuses: ApiStatus[] = [
-  "IDLE",
-  "PENDING",
-  "SUCCESS",
-  "ERROR",
-];
-
-export type ApiStatuses = Record<ApiStatus, ApiStatus>;
-
-export const apiStatus: ApiStatuses = {
-  IDLE,
-  PENDING,
-  SUCCESS,
-  ERROR,
-};
-
-type Statuses = Record<`is${Capitalize<Lowercase<ApiStatus>>}`, boolean>;
-
-const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
-
-const createStatuses = (currentStatus: ApiStatus): Statuses => {
-  const statuses = {} as Statuses;
-
-  for (const status of defaultApiStatuses) {
-    const normalizedStatus = capitalize(status.toLowerCase());
-    const normalizedStatusKey = `is${normalizedStatus}` as keyof Statuses;
-    statuses[normalizedStatusKey] = status === currentStatus;
-  }
-
-  return statuses;
-};
-
-export const useApiStatus = (currentStatus: ApiStatus = IDLE) => {
-  const [status, setStatus] = React.useState<ApiStatus>(currentStatus);
-  const statuses = React.useMemo(() => createStatuses(status), [status]);
-
-  return {
-    status,
-    setStatus,
-    ...statuses,
-  };
-};
