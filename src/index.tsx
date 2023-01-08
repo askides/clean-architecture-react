ebb9645fd40853bbc087fd23ad43bec7959662cd
itsrennyman <renato.pozzi.dev@gmail.com>
2023-01-08 16:34:50 +0100

    1 file modified, 2 deleted

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { TodosPage } from "./Presentation/Pages/TodosPage";
-import { UsersPage } from "./Presentation/Pages/UsersPage";
+import { TodosPage } from "./Presentation/UI/Pages/TodosPage";
+import { UsersPage } from "./Presentation/UI/Pages/UsersPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
